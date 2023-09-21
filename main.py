661cc2eb500d7d0214124d714de4c620fba63426
Alienkitten24 <felix.iov@outlook.com>
2023-09-21 19:40:22 -0400

    yeah

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from math import abs
+import random
 
 def calculate_compatibility(name1, name2):
-    pass
+    return random.randint(1, 100)
     #TODO Add compatibility calculator
 
 if __name__ == "__main__":
-    print("Hello world!")
+    print(calculate_compatibility(input("Name 1: "), input("Name 2: ")))
     #TODO Add your main section code here
