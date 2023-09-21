814d42a744490bcef84bbbbb77798034d0c4289a
Alienkitten24 <felix.iov@outlook.com>
2023-09-21 19:41:33 -0400

    yeah2

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import random
 
 def calculate_compatibility(name1, name2):
     return random.randint(1, 100)
-    #TODO Add compatibility calculator
 
 if __name__ == "__main__":
     print(calculate_compatibility(input("Name 1: "), input("Name 2: ")))
-    #TODO Add your main section code here
